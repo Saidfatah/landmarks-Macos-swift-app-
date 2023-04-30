a07f654b57201ebdbb440bcf37fbc6a80aa3d8d6
Saidfatah <said_designer@outlook.com>
2023-04-30 18:05:28 +0100

    Initial Commit

---
+++ landamrks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  landamrks
+//
+//  Created by said fatah on 30/4/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ landamrks/landamrksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landamrksApp.swift
+//  landamrks
+//
+//  Created by said fatah on 30/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct landamrksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
