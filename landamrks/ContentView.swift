e35cbff3f91a2b68d7d8292c176f1be8cd1696a6
Saidfatah <said_designer@outlook.com>
2023-04-30 19:03:01 +0100

    intial  commit .

--- landamrks/ContentView.swift
+++ landamrks/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+                .frame( height: 300)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .offset(y:-130.0)
+                .padding(.bottom,-130.0)
+            
+            VStack(alignment: .leading) {
+                Text("Hello, world wssup!")
+                    .font(.title)
+                
+                HStack {
+                    Text("oshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here.")
+            }.padding()
+            
+            Spacer()
+        }
     }
 }
 
